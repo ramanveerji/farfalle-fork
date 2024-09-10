843e76e0f94855aa938fbdf787452a6d3c8bfc0a
ramanveerji <ramandeepmatharu@gmail.com>
2024-09-11 01:11:56 +0530

    some changes

--- src/frontend/generated/types.gen.ts
+++ src/frontend/generated/types.gen.ts
@@ -68,7 +68,7 @@ export enum ChatModel {
   LLAMA_3_70B = "llama-3-70b",
   GPT_4O = "gpt-4o",
   GPT_4O_MINI = "gpt-4o-mini",
-  LLAMA3 = "llama3.1",
+  LLAMA3 = "llama3",
   GEMMA = "gemma",
   MISTRAL = "mistral",
   PHI3_14B = "phi3:14b",
